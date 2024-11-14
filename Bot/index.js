ca694b510b1783020aa38dd25257a12b2a7f85f4
Abenetwolde <abnetwoldedev@gmail.com>
2024-11-14 17:11:32 +0300

    psuh latest ocde

--- Bot/index.js
+++ Bot/index.js
@@ -7,8 +7,18 @@ bot.start((ctx) => {
         caption: "Caption",
         parse_mode: "Markdown",
         ...Markup.inlineKeyboard([
-          Markup.button.webApp("Click)", `https://0cdd-196-188-157-5.ngrok-free.app`),
+          Markup.button.webApp("Click)", `https://072f-196-188-157-5.ngrok-free.app`),
         ])
 }); 
 })
+bot.on("message", async (ctx) => {
+  console.log(ctx.message.web_app_data)
+  return ctx.reply(ctx.message.web_app_data.data)
+
+});
+// bot.on('callback_query',async (ctx) => {
+//   const webAppData = ctx.callbackQuery.data;  // This will contain the data sent from the Web App
+//   await ctx.reply('Data received from Web App:', webAppData);
+//   ctx.answerCbQuery();
+// });
 bot.launch(); 
